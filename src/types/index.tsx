7f1db1e83ea467583b4676a3a6b94e725e33f935
Administrator <lumengbit1@gmail.com>
2021-09-22 18:03:32 +1000

    add NewStoreState

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -2,3 +2,31 @@ export interface StoreState {
   languageName: string;
   enthusiasmLevel: number;
 }
+
+interface BrandingColors {
+  primary: string;
+}
+
+interface Agency {
+  brandingColors: BrandingColors;
+  logo: string;
+}
+
+interface Results {
+  price: string;
+  id: string;
+  mainImage: string;
+  agency: Agency;
+}
+
+interface Loading {
+  saved: boolean,
+  results: boolean,
+}
+
+export interface NewStoreState {
+  results: Results[],
+  saved: Results[],
+  errors: string,
+  loading: Loading,
+}
